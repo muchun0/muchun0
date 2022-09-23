584de7ebcaacbda510b87e4aa4fbcf80ed05c764
muchun <1120305692@qq.com>
2022-09-23 21:24:15 +0800

    api配置读取

--- apitest/conf/read.py
+++ apitest/conf/read.py
@@ -1,3 +1,10 @@
+from ast import main
 import configparser
+import os
 
-configparser.ConfigParser().getboolean()
+def readConfig(section,options):
+    config = configparser.ConfigParser()
+    config.read(os.path.join(os.path.dirname(__file__),'config.ini'))
+    return config[section][options]
+if __name__ == '__main__':
+    print(readConfig('ONLINE','url'))
